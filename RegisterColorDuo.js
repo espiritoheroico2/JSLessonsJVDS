cd4c2d417032e42c5c029d78a9d53d19e4fc754f
espiritoheroico2 <joao.vitor328@gmail.com>
2025-02-26 16:19:51 -0300

    26/02 lessons

--- RegisterColorDuo.js
+++ RegisterColorDuo.js
@@ -1,18 +1,18 @@
-`Em eletrônica, resistores têm bandas de cores que indicam seus valores. Neste exercício, você vai criar uma função que decodifica as duas primeiras bandas de cor de um resistor para formar um número.
+/*`Em eletrônica, resistores têm bandas de cores que indicam seus valores. Neste exercício, você vai criar uma função que decodifica as duas primeiras bandas de cor de um resistor para formar um número.
 
-Tarefa: Escreva uma função chamada decodedValue que:
+#Tarefa: Escreva uma função chamada decodedValue que:
 
-Recebe um array com duas strings representando cores (ex.: ["brown", "black"]).
-Retorna um número inteiro baseado nas posições dessas cores em uma lista padrão.
-A lista de cores é: ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"].
-Cada cor corresponde à sua posição na lista (ex.: "black" = 0, "brown" = 1, "red" = 2, etc.).
-O número final é formado concatenando os valores das duas cores como dezena e unidade (ex.: "brown" e "black" → 1 e 0 → 10).
-Casos de teste:
-
-decodedValue(["brown", "black"]) → 10
-decodedValue(["red", "violet"]) → 27
-decodedValue(["yellow", "green"]) → 45
-decodedValue(["black", "black"]) → 0`
+#Recebe um array com duas strings representando cores (ex.: ["brown", "black"]).
+#Retorna um número inteiro baseado nas posições dessas cores em uma lista padrão.
+#A lista de cores é: ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"].
+#Cada cor corresponde à sua posição na lista (ex.: "black" = 0, "brown" = 1, "red" = 2, etc.).
+#O número final é formado concatenando os valores das duas cores como dezena e unidade (ex.: "brown" e "black" → 1 e 0 → 10).
+#Casos de teste:
+#
+#decodedValue(["brown", "black"]) → 10
+#decodedValue(["red", "violet"]) → 27
+#decodedValue(["yellow", "green"]) → 45
+#decodedValue(["black", "black"]) → 0`*/
 
 function decodedValue(colors)
 {
@@ -21,11 +21,15 @@ function decodedValue(colors)
     let first = colorList.indexOf(colors[0]);
     let second = colorList.indexOf(colors[1]);
 
-    let result = first * 10 + second;
+    let result = (first * 10) + second;
+
+    console.log(result);
 
     return result;
+    
 
 }
+console.log("init");
 
 decodedValue(["brown", "black"]);
 decodedValue(["red", "violet"]);
